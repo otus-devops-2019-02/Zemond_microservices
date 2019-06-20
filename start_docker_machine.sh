bb9bf375067edf83d1b25d2ae67e0fbf416cdfec
zemond <3em0nd@gmail.com>
2019-06-20 23:45:11 +0300

    fix #2

---
+++ start_docker_machine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export GOOGLE_PROJECT=docker-242518
+
+#start machine 
+docker-machine create --driver google \
+--google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
+--google-machine-type n1-standard-1 \
+--google-disk-size 20 \
+--google-zone europe-west1-b docker-host
+#
+
+sleep 5
+eval $(docker-machine env docker-host)
+docker-machine ls
