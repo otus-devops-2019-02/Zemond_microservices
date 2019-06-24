158c39f9a4067a3363535397f3ec22f24739ba32
zemond <3em0nd@gmail.com>
2019-06-24 22:53:57 +0300

    fix bug #24

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -2,5 +2,5 @@
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
-
-docker build -t $USER_NAME/ui .
+export USER_NAME=zemond
+docker build -t $USER_NAME/post .
